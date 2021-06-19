1ed9abd4b4bffd3b52777e87400f8d6981e29ebd
Missy Maloney <missymaloney1@gmail.com>
2021-06-19 18:05:31 -0400

    first-commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+  $('#js-nav-toggle').click(function() {
+      $('#js-masthead').toggleClass('open-nav');
+    /*  $('#js-open-trigger').toggleClass('open-trigger');
+      $('#js-close-trigger').toggleClass('close-trigger');*/
+  });
